6c808a7395fae7610ec42d0323f8c33bc28fae33
Sam Coco <Samuel.HenriAC@outlook.com>
2019-07-12 17:17:28 +0100

    New thread added to client for socket input

--- src/network/Client.java
+++ src/network/Client.java
@@ -13,28 +13,56 @@ public class Client {
     private static PrintWriter output;
     private static Scanner scanner;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try(Socket socket = new Socket("localhost", 2000)){
-            socket.setSoTimeout(50);
             input = new Scanner(socket.getInputStream());
             output = new PrintWriter(socket.getOutputStream(), true);
             scanner = new Scanner(System.in);
-            System.out.print("ENTER USER NAME: ");
-            clientName = scanner.nextLine().toUpperCase();
-            output.println(clientName);
-            System.out.println(input.nextLine());
 
             while(true){
+                System.out.print("ENTER USERNAME: ");
+                clientName = scanner.nextLine().toUpperCase();
+                output.println(clientName);
+                receivedData = input.nextLine();
+                System.out.println(receivedData);
+                if(receivedData.equals(clientName + " has joined the chat!")){
+                    break;
+                }
+            }
+
+            boolean clientListenerOn = false;
+            while(true){
+                if(!clientListenerOn){
+                    new ClientListener().start();
+                    clientListenerOn = true;
+                }
                 System.out.print(clientName + " ->> ");
                 clientInput = scanner.nextLine();
                 output.println(clientInput);
-                if(input.hasNextLine()){
-                    System.out.println(input.nextLine());
+                if(clientInput.equals("``quit")){
+                    System.out.println("You have left the chat!");
+                    break;
                 }
             }
-        }catch(IOException e){
+        } catch(IOException e){
             System.out.println("CLIENT ERROR: " + e.getMessage());
         }
     }
 
+    public static class ClientListener extends Thread {
+
+        /**
+         * This method prevents the network IO blocking that would occur due to 'input.nextLine()' which blocks
+         * the main client thread as the client waits for data from the server. A new thread is started to listen
+         * to incoming data from the server instead.
+         */
+        @Override
+        public void run(){
+            if(input.hasNextLine()){
+                receivedData = input.nextLine();
+                System.out.println("\n"+receivedData);
+            }
+        }
+
+    }
 }
